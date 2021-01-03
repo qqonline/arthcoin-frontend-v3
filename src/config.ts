a4af20114c3633e1ff9b58c7fad782641b049694
Steven Enamakel <enamakel@vezures.com>
2021-01-03 17:24:50 +0300

    added development and ropsten addresses

--- src/config.ts
+++ src/config.ts
@@ -23,6 +23,27 @@ const configurations: { [env: string]: Configuration } = {
     refreshInterval: 10000,
     gasLimitMultiplier: 1.1,
   },
+  staging: {
+    chainId: ChainId.ROPSTEN,
+    etherscanUrl: 'https://etherscan.io',
+    defaultProvider: 'https://ropsten.infura.io/v3/69666afe933b4175afe4999170158a5f',
+    deployments: require('./basis-cash/deployments/deployments.ropsten.json'),
+    externalTokens: {
+      DAI: ['0x6B175474E89094C44Da98b954EedeAC495271d0F', 18],
+      yCRV: ['0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8', 18],
+      SUSD: ['0x57Ab1E02fEE23774580C119740129eAC7081e9D3', 18],
+
+      USDC: ['0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 6],
+      USDT: ['0xdAC17F958D2ee523a2206206994597C13D831ec7', 6],
+      'BAC_DAI-UNI-LPv2': ['0xd4405F0704621DBe9d4dEA60E128E0C3b26bddbD', 18],
+      'BAS_DAI-UNI-LPv2': ['0x0379dA7a5895D13037B6937b109fA8607a659ADF', 18],
+    },
+    baseLaunchDate: new Date('2020-11-26T00:00:00Z'),
+    bondLaunchesAt: new Date('2020-12-03T15:00:00Z'),
+    boardroomLaunchesAt: new Date('2020-12-11T00:00:00Z'),
+    refreshInterval: 10000,
+    gasLimitMultiplier: 1.1,
+  },
   production: {
     chainId: ChainId.MAINNET,
     etherscanUrl: 'https://etherscan.io',
@@ -104,4 +125,6 @@ export const bankDefinitions: { [contractName: string]: BankInfo } = {
   },
 };
 
-export default configurations[process.env.NODE_ENV || "production"];
+// export default configurations[process.env.NODE_ENV || "production"];
+
+export default configurations["staging"];
