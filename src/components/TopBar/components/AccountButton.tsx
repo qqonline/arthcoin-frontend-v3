396931dee0281775c78aec9289d0651941fcb7b7
Steven Enamakel <enamakel@vezures.com>
2021-01-25 00:46:59 +0300

    added address to account button

--- src/components/TopBar/components/AccountButton.tsx
+++ src/components/TopBar/components/AccountButton.tsx
@@ -7,6 +7,21 @@ import AccountModal from './AccountModal';
 import AccountBalanceWalletOutlinedIcon from '@material-ui/icons/AccountBalanceWalletOutlined';
 interface AccountButtonProps {}
 
+const truncateMiddle = function (fullStr: string, strLen: number, separator: string) {
+  if (fullStr.length <= strLen) return fullStr;
+
+  separator = separator || '...';
+
+  var sepLen = separator.length,
+      charsToShow = strLen - sepLen,
+      frontChars = Math.ceil(charsToShow/2),
+      backChars = Math.floor(charsToShow/2);
+
+  return fullStr.substr(0, frontChars) +
+         separator +
+         fullStr.substr(fullStr.length - backChars);
+};
+
 const AccountButton: React.FC<AccountButtonProps> = (props) => {
   const [showModal, toggleModal] = React.useState(false);
 
@@ -19,7 +34,7 @@ const AccountButton: React.FC<AccountButtonProps> = (props) => {
         {!account ? (
           <Button onClick={() => connect('injected')} size="sm" text="Connect" />
         ) : (
-          <ButtonColored onClick={() => toggleModal(true)} size="sm" text="Connected">
+          <ButtonColored onClick={() => toggleModal(true)} size="sm" text={truncateMiddle(account , 15, '.....')}>
             <AccountBalanceWalletOutlinedIcon className="margin-right-10 font20" />
           </ButtonColored>
         )}
