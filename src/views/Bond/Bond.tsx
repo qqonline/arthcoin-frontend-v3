9b6b560fec84327a8aac85f0b9b256bc46b9d16c
Steven Enamakel <enamakel@vezures.com>
2021-01-26 16:40:16 +0300

    fixedtypo

--- src/views/Bond/Bond.tsx
+++ src/views/Bond/Bond.tsx
@@ -122,9 +122,7 @@ const Bond: React.FC = () => {
                           : `ARTH is below its target price and ARTHB can be purchased`
                       }
                       onExchange={handleBuyBonds}
-                      disassssssssssssssssssssssssssssssssssssssssssssssssssssbled={
-                        !isBondPurchasable || bondsAvailableForPurchase.eq(0)
-                      }
+                      disabled={!isBondPurchasable || bondsAvailableForPurchase.eq(0)}
                     />
                   </Grid>
                   <Grid item xs={12} md={6} lg={6} xl={6}>
