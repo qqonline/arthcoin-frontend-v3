13c091a0f013a01e759668c8c91181c604a4fac9
Steven Enamakel <enamakel@vezures.com>
2021-03-03 17:52:50 +0300

    update boardroom

--- src/views/Distributions/BoardroomV2/components/BoardroomStaked.tsx
+++ src/views/Distributions/BoardroomV2/components/BoardroomStaked.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import styled from 'styled-components';
 
+import LinkButton from '../../../../components/Button/LinkButton';
 import Button from '../../../../components/Button';
 import CardWithTitle from '../../../../components/CardWithTitle';
-import useApprove from '../../../../hooks/useApprove';
 import useModal from '../../../../hooks/useModal';
 import useTokenBalance from '../../../../hooks/useTokenBalance';
 import { getDisplayBalance } from '../../../../utils/formatBalance';
@@ -26,30 +26,28 @@ const Stake = ({ vault }: { vault: VaultInfo }) => {
   const basisCash = useBasisCash();
 
   const stakingToken =
-    vault.depositTokenName === 'MAHA'
-      ? basisCash.MAHA
-      : vault.depositTokenName === 'ARTH'
-        ? basisCash.ARTH
-        : vault.depositTokenName === 'ARTH_DAI-MLP-LPv1'
-          ? basisCash.arthDai
-          : vault.depositTokenName === 'ARTH_DAI'
-            ? basisCash.arthDai
-            : basisCash.externalTokens[vault.depositTokenName];
-
-  // eslint-disable-next-line
-  const [approveStatus, approve] = useApprove(
-    stakingToken,
-    basisCash.getBoardroomVault(vault.kind)?.address,
-  );
+    vault.depositTokenName === 'MAHA' ? basisCash.MAHA
+      : vault.depositTokenName === 'ARTH' ? basisCash.ARTH
+        : vault.depositTokenName === 'ARTH_DAI-MLP-LPv1' ? basisCash.arthDai
+          : basisCash.externalTokens[vault.depositTokenName];
 
   const logos = [];
-  const title = vault.depositTokenName === 'ARTH' ? 'ARTH-DAI' : vault.depositTokenName;
+
   if (vault.depositTokenName === 'ARTH_DAI-UNI-LPv2') logos.push('ARTH', 'DAI');
   else if (vault.depositTokenName === 'ARTH_DAI-MLP-LPv1') logos.push('ARTH', 'DAI');
   else if (vault.depositTokenName === 'MAHA_ETH-UNI-LPv2') logos.push('MAHA', 'ETH');
   else if (vault.depositTokenName === 'ARTH_DAI-MAHA-LP') logos.push('ARTH', 'DAI');
-  else if (vault.depositTokenName === 'ARTH') logos.push('ARTH', 'DAI');
   else logos.push(stakingToken.symbol);
+
+
+  const finalDepositTokenName = useMemo(() => {
+    const depositTokenName = vault.depositTokenName.startsWith('ARTH_DAI') ? 'ARTH-DAI' : vault.depositTokenName
+
+    const pool = vault.depositTokenName.includes('MLP') ? 'MahaSwap LP' :
+      vault.depositTokenName.includes('UNI') ? "Uniswap LP" : ""
+
+    return `${depositTokenName} ${pool}`
+  }, [vault.depositTokenName])
 
 
   const tokenBalance = useTokenBalance(stakingToken);
@@ -58,7 +56,7 @@ const Stake = ({ vault }: { vault: VaultInfo }) => {
   const { onBond } = useStakeToVault(vault);
   const { onWithdraw } = WithdrawShareFromVault(vault);
   const { onUnbond } = useUnbondFromVault(vault);
-  // eslint-disable-next-line
+
   const [onPresentBond, onDismissBond] = useModal(
     <BondModal
       max={tokenBalance}
@@ -71,7 +69,6 @@ const Stake = ({ vault }: { vault: VaultInfo }) => {
     />,
   );
 
-  // eslint-disable-next-line
   const [onPresentUnBond, onDismissUnbond] = useModal(
     <UnbondModal
       max={stakedBalance}
@@ -95,53 +92,62 @@ const Stake = ({ vault }: { vault: VaultInfo }) => {
         <StyledCardIcon>
           {logos && logos.length > 0 && (
             <LogoContainer>
-              {logos.map((l: any, idx: number) => (
-                <TokenSymbol key={idx} symbol={l} size={36} left={idx > 0 ? -5 : 0} />
-              ))}
+              {logos.map((l: any, idx: number) => <TokenSymbol symbol={l} size={36} left={idx > 0 ? -5 : 0} />)}
             </LogoContainer>
           )}
         </StyledCardIcon>
-        <StyleLabel>{`${title} Staked`} </StyleLabel>
+        <StyleLabel>{`${finalDepositTokenName} Staked`} </StyleLabel>
       </StyledCardHeader>
       <StyledCardContent>
         <StyledCardContentInner>
-          <StyledFieldLabel>Your liquidity</StyledFieldLabel>
+          <StyledFieldLabel>Your Staked balance</StyledFieldLabel>
           <StyledValue>
             <Bolded>
               {getDisplayBalance(stakedBalance)}
             </Bolded>
             <Darked>{vault.depositTokenName}</Darked>
           </StyledValue>
+
+          {/* TODO */}
           {/* <StyledFieldLabel>
             {`${tokenBalance} ${logos[0]} + 0 DAI `} Staked
           </StyledFieldLabel> */}
 
-          <StyledRow>
-            <Button to={''} variant="rounded">
-              Provide liquidity on MahaSwap
-            <CallMade fontSize='small' style={{ transform: "scale(0.7)", marginLeft: 3 }} />
-            </Button>
-          </StyledRow>
+          {
+            vault.depositTokenName.includes('MLP') && (
+              <StyledRow>
+                <LinkButton href={'https://mahaswap.com/#/add/0x0E3cC2c4FB9252d17d07C67135E48536071735D9/0x6B175474E89094C44Da98b954EedeAC495271d0F'} variant="rounded">
+                  Earn LP tokens from MahaSwap
+                  <CallMade fontSize='small' style={{ transform: "scale(0.7)", marginLeft: 3 }} />
+                </LinkButton>
+              </StyledRow>
+            )
+          }
         </StyledCardContentInner>
 
-        <StyledValue>
-          {unbondedAmount.gt(-1) && (
-            <ProgressCountdown
-              base={depositDate}
-              deadline={endDate}
-              description={`Withdraw Period`}
-              tooltip="Withdraw Period Tooltip"
-            />
-          )}
-        </StyledValue>
-        <p style={{ color: '#fff9', fontSize: 13, fontWeight: 300, textAlign: 'center' }}>
-          500 ARTH+DAI requested for withdraw
-        </p>
+
+        {
+          unbondedAmount.gt(0) && (
+            <>
+              <StyledValue>
+                <ProgressCountdown
+                  base={depositDate}
+                  deadline={endDate}
+                  description={`Withdraw Period`}
+                  tooltip="Withdraw Period Tooltip"
+                />
+              </StyledValue>
+              <p style={{ color: '#fff9', fontSize: 13, fontWeight: 300, textAlign: 'center' }}>
+                {getDisplayBalance(unbondedAmount)} {finalDepositTokenName} requested for withdraw
+              </p>
+            </>
+          )
+        }
 
         <Grid container spacing={2} justify="center">
           <Grid item md={6} lg={6}>
             <Button
-              onClick={onWithdraw}
+              onClick={onPresentUnBond}
               variant="outlined"
               disabled={endDate.getTime() > Date.now()}
               text="Withdraw"
@@ -150,10 +156,22 @@ const Stake = ({ vault }: { vault: VaultInfo }) => {
           <Grid item md={6} lg={6}>
             <Button
               // disabled={approveStatus !== ApprovalState.NOT_APPROVED}
-              onClick={approve}
+              onClick={onPresentBond}
               text="Deposit"
             />
           </Grid>
+          {
+            unbondedAmount.gt(0) && (
+              <Grid item md={12}>
+                <Button
+                  onClick={onWithdraw}
+                  variant="outlined"
+                  disabled={endDate.getTime() > Date.now()}
+                  text="Process Withdrawal"
+                />
+              </Grid>
+            )
+          }
         </Grid>
       </StyledCardContent>
     </CardWithTitle>
@@ -214,6 +232,10 @@ const StyledFieldLabel = styled.div`
   color: rgba(255, 255, 255, 0.64);
 `;
 
+// const StyledActionSpacer = styled.div`
+//   height: ${(props) => props.theme.spacing[4]}px;
+//   width: ${(props) => props.theme.spacing[4]}px;
+// `;
 
 const StyledCardContent = styled.div`
   display: flex;
@@ -232,5 +254,11 @@ const StyledCardContentInner = styled.div`
   flex: 1;
 `;
 
+// const StyledDesc = styled.span`
+//   color: ${(props) => props.theme.color.grey[500]};
+//   font-weight: 400;
+//   font-size: 12px;
+//   text-align: center;
+// `;
 
 export default Stake;
