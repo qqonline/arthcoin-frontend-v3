a0ffb5b8228103a362ebed06d70a2b0455182c22
Mithil Jain <mithiljain8@gmail.com>
2021-04-19 16:45:05 +0530

    unwanted code

--- src/views/Genesis/BoardroomsV2.tsx
+++ src/views/Genesis/BoardroomsV2.tsx
@@ -5,29 +5,18 @@ import useBasisCash from '../../hooks/useBasisCash';
 import Grid from '@material-ui/core/Grid';
 import InfoIcon from '@material-ui/icons/Info';
 import Button from '../../components/Button';
-import Modal from './components/modal';
 import arrowDown from '../../assets/svg/arrowDown.svg'
-import plus from '../../assets/svg/plus.svg'
-import warning from '../../assets/svg/warning.svg'
-import InputContainer from './components/InputContainer';
-import { Checkbox, CheckboxProps, createStyles, Divider, FormControlLabel, IconButton, LinearProgress, makeStyles, Slider, Snackbar, Theme, withStyles } from '@material-ui/core';
+import { Checkbox, CheckboxProps, createStyles, Divider, LinearProgress, makeStyles, Slider, Theme, withStyles } from '@material-ui/core';
 import TransparentInfoDiv from './components/InfoDiv';
-import CheckIcon from '@material-ui/icons/Check';
-import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
-import Tooltip from '@material-ui/core/Tooltip';
-import theme from '../../theme';
 import HtmlTooltip from '../../components/HtmlTooltip';
 import CustomInputContainer from '../../components/CustomInputContainer';
 import CustomModal from '../../components/CustomModal';
 import { withSnackbar, WithSnackbarProps } from 'notistack';
 import { CustomSnack } from '../../components/SnackBar';
-import CloseIcon from '../../assets/img/CloseIcon.svg';
 import CustomSuccessModal from '../../components/CustomSuccesModal';
 import BondingDiscount from '../Genesis/components/BondingDiscount';
 import UnderstandMore from './components/UnderstandMore';
-import PageHeader from '../../components/PageHeader';
 import { useMediaQuery } from 'react-responsive';
-import { KeyboardArrowRight } from '@material-ui/icons';
 
 
 // const HtmlTooltip = withStyles((theme1: Theme) => ({
@@ -41,7 +30,7 @@ import { KeyboardArrowRight } from '@material-ui/icons';
 //   },
 // }))(Tooltip);
 
-const OrangeCheckBox = withStyles({
+withStyles({
   root: {
     color: 'rgba(255, 255, 255, 0.32)',
     '&$checked': {
@@ -53,7 +42,7 @@ const OrangeCheckBox = withStyles({
   },
 })((props: CheckboxProps) => <Checkbox {...props} />);
 
-const useSliderStyles = makeStyles((theme: Theme) =>
+makeStyles((theme: Theme) =>
   createStyles({
     root: {
       width: '100%',
@@ -66,15 +55,11 @@ const useSliderStyles = makeStyles((theme: Theme) =>
 );
 
 
-function valuetext(value: number) {
-  return `${value}`;
-}
-
 // function valueLabelFormat(value: number) {
 //   return marks.findIndex((mark: any) => mark.value === value) + 1;
 // }
 
-const PrettoRestrictSlider = withStyles({
+withStyles({
   root: {
     // color: 'white',
     height: 15,
@@ -132,7 +117,6 @@ const PrettoRestrictSlider = withStyles({
   },
 
 })(Slider);
-const DEFAULT_CALC = 1440;
 
 // const HtmlTooltip = withStyles((theme) => ({
 //   tooltip: {
@@ -184,84 +168,28 @@ const BorderLinearProgress = withStyles((theme: Theme) =>
 const Boardrooms = (props: WithSnackbarProps) => {
   useEffect(() => window.scrollTo(0, 0), []);
   const basisCash = useBasisCash();
-  const colletralRatio = 86;
+
   const [mintColl, setCollateralValue] = useState<number>(0.00)
-  const [mintArthxShare, setArthxShare] = useState<number>(0.00)
-  const [balance, setBalance] = useState<number>(0)
-  const [mintReceive, setReceive] = useState<number>(0)
-  const [redeemAmount, setRedeemAmount] = useState<number>(0)
-  const [finalValue, setFinalValue] = useState<number>(100)
-  const [calcDuration, setDuration] = useState<number>(DEFAULT_CALC)
-  const [currentCounter, setCurrentCounter] = useState<number>(1000)
+  const [arthValue, setArthValue] = useState<number>(0.00)
+  const [balance] = useState<number>(0)
+
   const [type, setType] = useState<'Commit' | 'Swap'>('Commit')
   const [openModal, setOpenModal] = useState<0 | 1 | 2>(0);
-  const [checked, setChecked] = React.useState(false);
-  const [testnetDiv, showDiv] = React.useState(true);
-  const sliderClasses = useSliderStyles();
-  const [sliderValue, setSliderValue] = React.useState(1);
+
+
   const [successModal, setSuccessModal] = useState<boolean>(false)
-  const isMobile = useMediaQuery({ 'maxWidth': '600px' })
-  const [selectedCollateralCoin, setSelectedCollateralCoin] = useState<string>('USDT')
+  useMediaQuery({ 'maxWidth': '600px' });
+  const [selectedCollateralCoin, setSelectedCollateralCoin] = useState<string>('MAHA')
   const [CollateraldropDownValues, setCollateralDropDownValues] = useState<string[]>([]);
-  const defaultCollateralDropdownValues = ['MAHA', 'ARTH', 'USDT', 'USDC', 'ETH', 'WBTC'];
-  let arr: string[];
+  const defaultCollateralDropdownValues = ['MAHA','WBTC', 'USDT', 'USDC', 'ETH'];
+
   useEffect(() => {
+    let arr: string[];
     arr = defaultCollateralDropdownValues.filter(e => e !== selectedCollateralCoin);
     setCollateralDropDownValues(arr);
   }, [selectedCollateralCoin])
 
-  const [selectedReceiveRedeemCoin, setSelectedReceiveRedeemCoin] = useState<string>('USDT')
-  const [ReceiveRedeemdropDownValues, setReceiveRedeemDropDownValues] = useState<string[]>([]);
-  const defaultReceiveRedeemDropdownValues = ['MAHA', 'ARTH', 'USDT', 'USDC', 'ETH', 'WBTC'];
-  let temp: string[];
-  useEffect(() => {
-    temp = defaultReceiveRedeemDropdownValues.filter(e => e !== selectedReceiveRedeemCoin);
-    setReceiveRedeemDropDownValues(temp);
-  }, [selectedReceiveRedeemCoin]);
-
-  // const onBuyColletralValueChange = async (val: string) => {
-  //   const valueInNumber = Number(val.replace(/[^0-9]/g, ''))
-  //   setCollateralValue(valueInNumber);
-  //   let arthxShareTemp = await ((100 * valueInNumber)/colletralRatio) * ((100 - colletralRatio)/100)
-  //   setArthxShare(arthxShareTemp);
-  //   setReceive(arthxShareTemp + valueInNumber);
-  // }
-
-  // const onARTHXValueChange = async (val: string) => {
-  //   const valueInNumber = Number(val.replace(/[^0-9]/g, ''))
-  //   setArthxShare(valueInNumber);
-  //   let colletralTemp = await ((100 * valueInNumber)/(100-colletralRatio)) * ((colletralRatio)/100)
-  //   setCollateralValue(colletralTemp);
-  //   setReceive(colletralTemp + valueInNumber);
-  // }
-
-  // const TestNetSnack = () => {
-  //   return (
-  //     <TestNetDiv>
-  //       <div />
-  //       <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-  //         <img src={warning} height={24} />
-  //         <span style={{ marginLeft: 5 }}>
-  //           Please make sure that you are connected to matic mumbai TESTnet.
-  //         </span>
-  //       </div>
-  //       <div style={{cursor: 'pointer'}}>
-  //         <img src={CloseIcon} height={20} onClick={() => { showDiv(false) }} />
-  //       </div>
-  //     </TestNetDiv>
-  //   )
-  // }
-  // const isLaunched = Date.now() >= config.boardroomLaunchesAt.getTime();
   if (!basisCash) return <div />;
-  const handleCheck = (event: any) => {
-    // console.log('check trig', event.target.checked)
-    setChecked(event.target.checked);
-  };
-  const handleSliderChange = (event: any, value: any) => {
-    console.log('check trig', value)
-    setSliderValue(value);
-    setDuration(DEFAULT_CALC - value * value)
-  };
 
   const understandMore = [
     'Commited collateral will go to ARTH protocol',
@@ -283,37 +211,20 @@ const Boardrooms = (props: WithSnackbarProps) => {
         <>
           <TransparentInfoDiv
             labelData={`Your amount`}
-            rightLabelUnit={'USDT'}
+            rightLabelUnit={type === 'Commit'? selectedCollateralCoin: 'ARTH'}
             rightLabelValue={mintColl.toString()}
           />
 
-          <Divider
-            style={{
-              background: 'rgba(255, 255, 255, 0.08)',
-              margin: '15px 0px'
-            }}
-          // variant={'middle'}
-          />
+          <Divider style={{ background: 'rgba(255, 255, 255, 0.08)', margin: '15px 0px' }} />
 
           <TransparentInfoDiv
             labelData={`You will receive`}
-            // labelToolTipData={'testing'}
-            rightLabelUnit={'ARTH'}
+            rightLabelUnit={'ARTHX'}
             rightLabelValue={'1000.00'}
           />
 
-          <div style={{
-            flexDirection: isMobile ? 'column-reverse' : 'row',
-            display: 'flex',
-            width: '100%',
-            marginTop: '10%',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            marginBottom: 8
-          }}
-          >
-            <div style={{ flex: 1, width: isMobile ? '100%' : '50%', marginTop: isMobile ? 10 : 0, marginRight: !isMobile ? 15 : 0 }}>
-
+          <Grid container spacing={2} style={{ marginTop: '32px' }}>
+            <Grid item lg={6} md={6} sm={12} xs={12}>
               <Button
                 variant={'transparent'}
                 text="Cancel"
@@ -326,8 +237,8 @@ const Boardrooms = (props: WithSnackbarProps) => {
                   props.enqueueSnackbar('timepass', options)
                 }}
               />
-            </div>
-            <div style={{ width: isMobile ? '100%' : '50%' }}>
+            </Grid>
+            <Grid item lg={6} md={6} sm={12} xs={12}>
               <Button
                 text={type === 'Commit' ? 'Commit Collateral' : 'Swap ARTH'}
                 // textStyles={{ color: '#F5F5F5' }}
@@ -343,8 +254,8 @@ const Boardrooms = (props: WithSnackbarProps) => {
                   }, 3000)
                 }}
               />
-            </div>
-          </div>
+            </Grid>
+          </Grid>
         </>
       </CustomModal>
       <div style={{}}>
@@ -437,14 +348,14 @@ const Boardrooms = (props: WithSnackbarProps) => {
                     ILabelValue={'Enter ARTH'}
                     IBalanceValue={`Balance ${balance}`}
                     ILabelInfoValue={''}
-                    value={mintColl.toString()}
-                    DefaultValue={mintColl.toString()}
+                    value={arthValue.toString()}
+                    DefaultValue={arthValue.toString()}
                     LogoSymbol={'ARTH'}
                     hasDropDown={false}
                     SymbolText={'ARTH'}
                     inputMode={'numeric'}
                     setText={(val: string) => {
-                      setCollateralValue(Number(val.replace(/[^0-9]/g, '')))
+                      setArthValue(Number(val.replace(/[^0-9]/g, '')))
                     }}
                   />
                 }
@@ -518,12 +429,7 @@ const CustomInfoCardDetails = styled.div`
   }
 `
 
-const OneLine = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`
+
 
 const PageHeading = styled.p`
   font-family: Syne;
@@ -545,7 +451,7 @@ const HeaderSpan = styled.span`
   font-size: 18px;
   line-height: 24px;
   display: flex;
-  margin: 0px 0px 0px 8px;
+  margin: 0 0 0 8px;
   color: #FFFFFF;
 `;
 
@@ -564,30 +470,8 @@ const PageSubHeading = styled.div`
 
 `
 const ToolTipFont = styled.p`
-  padding: 0px;
-  margin: 0px;
-`
-const TestNetDiv = styled.div`
-  width: 100%;
-  background: #BA1E38;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  text-align:center;
-  font-family: Inter;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 24px;
-  color: #FFFFFF;
-  opacity: 0.88;
-  padding: 10px 25px;
-`;
-
-const TcContainer = styled.div`
-  margin-top: 18px;
-  margin-bottom: 18px;
+  padding: 0;
+  margin: 0;
 `
 
 const OneLineInputwomargin = styled.div`
@@ -597,55 +481,9 @@ const OneLineInputwomargin = styled.div`
   justify-content: flex-start;
 `
 
-const StyledTableHeaderTextCenter = styled.h6`
-  font-size: 12px;
-  font-weight: 600;
-  color: ${(props) => props.theme.color.grey[600]};
-  margin: 10px 30px;
-  text-align: center;
-`;
-
-const InfoSpan = styled.span`
-font-family: Inter;
-font-style: normal;
-font-weight: 600;
-font-size: 14px;
-line-height: 20px;
-color: rgba(255, 255, 255, 0.64);
-// margin: 10px 30px;
-text-align: center;
-`;
-
 const LeftTopCard = styled.div`
   background: linear-gradient(180deg, #48423E 0%, #373030 100%);
   border-radius: 12px;
-`
-
-const RightTopCard = styled.div`
-  background: rgba(255, 255, 255, 0.02);
-  backdrop-filter: blur(21px);
-  border-radius: 12px;
-  padding: 32px;
-`
-
-const RightBottomCard = styled.div`
-  margin-top: 24px;
-  background: rgba(255, 255, 255, 0.02);
-  backdrop-filter: blur(21px);
-  border-radius: 12px;
-  padding: 32px;
-`
-
-const RightBottomCardTitle = styled.div`
-  padding: 0px;
-  margin: 0px;
-  font-family: Inter;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 24px;
-  color: rgba(255, 255, 255, 0.88);
-
 `
 
 const LeftTopCardHeader = styled.div`
@@ -687,11 +525,6 @@ const TabText = styled.span`
   text-align: center;
   color: rgba(255, 255, 255, 0.88);
 `
-const StakingDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 5px 0px 0px 0px;
-`;
 
 const ActiveTab = styled.div`
   position: absolute;
@@ -774,206 +607,4 @@ const TagChips = styled.div`
 
 `
 
-const InputLabelSpanRight = styled.span`
-  font-family: Inter;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 14px;
-  line-height: 20px;
-  color: rgba(255, 255, 255, 0.88);
-  margin-right: 5px;
-`
-
-const InputLabel = styled.p`
-  font-family: Inter;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 14px;
-  color: rgba(255, 255, 255, 0.64);
-  margin: 0px;
-`
-const StyledTableHeaderTextRight = styled.h6`
-  font-size: 12px;
-  font-weight: 600;
-  color: ${(props) => props.theme.color.grey[600]};
-  margin: 10px 10px;
-`;
-
-const InternalSpan = styled.span`
-font-family: Inter;
-font-style: normal;
-font-weight: 600;
-font-size: 12px;
-line-height: 150%;
-letter-spacing: 0.08em;
-text-transform: uppercase;
-color: #FFFFFF;
-`
-
-const InputNoDisplay = styled.span`
-  background: rgba(255, 255, 255, 0.08);
-  border-radius: 4px;
-  padding: 2px 10px;
-  height: 25px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 0px 0px 0px 8px;
-`
-
-const LabelDiv = styled.div`
-// background: rgba(255, 255, 255, 0.08);
-// border-radius: 6px;
-// padding: 6px 4px;
-height: fit-content;
-justify-content: space-between;
-display: flex;
-align-items: center;
-// margin: 5px 0px 0px 0px;
-`;
-
-const LabelInfo = styled.div`
-// background: rgba(255, 255, 255, 0.08);
-// border-radius: 6px;
-padding: 3px 4px;
-height: fit-content;
-// justify-content: space-between;
-display: flex;
-align-items: center;
-font-family: Inter;
-font-style: normal;
-font-weight: 300;
-font-size: 12px;
-line-height: 130%;
-color: rgba(255, 255, 255, 0.88);
-`;
-
-const LabelInfoText = styled.div`
-font-family: Inter;
-font-style: normal;
-font-weight: 600;
-font-size: 14px;
-line-height: 20px;
-text-align: right;
-color: rgba(255, 255, 255, 0.88);
-`;
-
-const TimeSpan = styled.div`
-font-family: Inter;
-font-style: normal;
-font-weight: 300;
-font-size: 12px;
-line-height: 130%;
-color: rgba(255, 255, 255, 0.88);
-`;
-
-const LabelInfoData = styled.div`
-// background: yellow;
-padding: 3px 4px;
-// height: fit-content;
-width: fit-content;
-justify-content: space-between;
-display: flex;
-flex-direction: row;
-align-items: center;
-font-family: Inter;
-font-style: normal;
-font-weight: 300;
-font-size: 12px;
-line-height: 130%;
-color: rgba(255, 255, 255, 0.88);
-`;
-
-const LabelInfoDataChip = styled.div`
-background: rgba(255, 255, 255, 0.08);
-border-radius: 4px;
-padding: 3px 4px;
-height: fit-content;
-// justify-content: space-between;
-display: flex;
-align-items: center;
-font-family: Inter;
-font-style: normal;
-font-weight: 300;
-font-size: 12px;
-line-height: 130%;
-margin: 0px 2px;
-color: rgba(255, 255, 255, 0.64);
-`;
-
-const LabelInfoDataChipText = styled.div`
-font-family: Inter;
-font-style: normal;
-font-weight: 600;
-font-size: 12px;
-line-height: 150%;
-letter-spacing: 0.08em;
-text-transform: uppercase;
-color: rgba(255, 255, 255, 0.64);
-`;
-
-const InfoDiv = styled.div`
-background: rgba(255, 255, 255, 0.08);
-border-radius: 6px;
-padding: 6px 4px;
-height: fit-content;
-justify-content: space-between;
-display: flex;
-align-items: center;
-`;
-
-// const TransparentInfoDiv = styled.div`
-// // background: rgba(255, 255, 255, 0.08);
-// // border-radius: 6px;
-// // padding: 6px 4px;
-// height: fit-content;
-// justify-content: space-between;
-// display: flex;
-// align-items: center;
-// `;
-
-const InfoTitle = styled.div`
-padding: 6px 4px;
-height: fit-content;
-display: flex;
-align-items: center;
-font-family: Inter;
-font-style: normal;
-font-weight: 600;
-font-size: 14px;
-line-height: 20px;
-color: rgba(255, 255, 255, 0.88);
-`;
-
-const CheckboxDiv = styled.div`
-background: rgba(255, 255, 255, 0.08);
-border-radius: 6px;
-padding: 5px 0px 0px 5px;
-display: flex;
-justify-content: center;
-align-items: center;
-font-family: Inter;
-font-style: normal;
-font-weight: 600;
-font-size: 14px;
-line-height: 20px;
-text-align: center;
-color: rgba(255, 255, 255, 0.88);
-margin: 15px 0px 0px 0px;
-`;
-
-const CurrencyTag = styled.div`
-padding: 6px 4px;
-width: 85px;
-justify-content: space-around;
-height: fit-content;
-display: flex;
-align-items: center;
-font-family: Inter;
-font-style: normal;
-font-weight: 600;
-font-size: 14px;
-line-height: 20px;
-color: rgba(255, 255, 255, 0.64);
-`;
 export default withSnackbar(Boardrooms);
