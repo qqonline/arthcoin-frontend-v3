eab7bdf8ef873553e1d9fbb5552a5b276743fd2c
Steven Enamakel <enamakel@vezures.com>
2021-05-01 01:31:29 +0300

    update ABIs

--- src/basis-cash/deployments/abi.ts
+++ src/basis-cash/deployments/abi.ts
@@ -1,15 +1,18 @@
 export const ArthController = require('./abi/ArthController.json')
-export const ARTHShares = require('./abi/ARTHShares.json')
-export const IERC20 = require('./abi/IERC20.json')
-export const MahaToken = require('./abi/MahaToken.json')
-export const UniswapPairOracle = require('./abi/UniswapPairOracle.json')
-export const UniswapV2Router02 = require('./abi/UniswapV2Router02.json')
 export const ArthPool = require('./abi/ArthPool.json')
+export const ArthPoolRouter = require('./abi/ArthPoolRouter.json')
+export const ARTHShares = require('./abi/ARTHShares.json')
 export const ARTHStablecoin = require('./abi/ARTHStablecoin.json')
+export const BoostedStaking = require('./abi/BoostedStaking.json')
+export const GMUOracle = require('./abi/GMUOracle.json')
+export const IERC20 = require('./abi/IERC20.json')
 export const IWETH = require('./abi/IWETH.json')
-export const StakingRewards = require('./abi/StakingRewards.json')
-export const UniswapV2Factory = require('./abi/UniswapV2Factory.json')
+export const MahaToken = require('./abi/MahaToken.json')
 export const Multicall = require('./abi/Multicall.json')
+export const SimpleOracle = require('./abi/SimpleOracle.json')
+export const UniswapPairOracle = require('./abi/UniswapPairOracle.json')
+export const UniswapV2Factory = require('./abi/UniswapV2Factory.json')
+export const UniswapV2Router02 = require('./abi/UniswapV2Router02.json')
 
 
 type IABIS = {
@@ -17,19 +20,21 @@ type IABIS = {
 }
 
 const ret: IABIS = {
-  Multicall,
   ArthController,
-  ARTHShares,
-  IERC20,
-  MahaToken,
-  UniswapPairOracle,
-  UniswapV2Router02,
   ArthPool,
+  ArthPoolRouter,
+  ARTHShares,
   ARTHStablecoin,
+  BoostedStaking,
+  GMUOracle,
+  IERC20,
   IWETH,
-  StakingRewards,
+  MahaToken,
+  Multicall,
+  SimpleOracle,
+  UniswapPairOracle,
   UniswapV2Factory,
-
+  UniswapV2Router02,
 }
 
 export default ret
