7bb6f2a915d7662600c0734a7579862611600251
Yash Agrawal <yashagrawal18@outlook.com>
2021-05-26 18:28:34 +0530

    Update some more hooks

--- src/hooks/callbacks/staking/useStakingClaim.ts
+++ src/hooks/callbacks/staking/useStakingClaim.ts
@@ -1,22 +1,34 @@
 import { useCallback } from 'react';
 
 import useCore from '../../useCore';
+import { useAddPopup } from '../../../state/application/hooks';
 import { useTransactionAdder } from '../../../state/transactions/hooks';
 
 export default function (stakingContract: string) {
   const core = useCore();
+  const addPopup = useAddPopup();
   const addTransaction = useTransactionAdder();
 
   const action = useCallback(async (callback: () => void): Promise<void> => {
     const contract = core.contracts[stakingContract];
-    const response = await contract.getRewardAndDistribute();
     
-    addTransaction(response, {
-      summary: `Claim Rewards`
-    });
+    try {
+      const response = await contract.getRewardAndDistribute();
 
-    if (callback) callback();
-  }, [core.contracts, stakingContract, addTransaction]);
+      addTransaction(response, {
+        summary: `Claim Rewards`
+      });
+
+      if (callback) callback();
+    } catch(e) {
+      addPopup({
+        error: {
+          message: e.data.message,
+          stack: e.stack
+        }
+      });
+    }
+  }, [core.contracts, addPopup, stakingContract, addTransaction]);
 
   return action;
 }
--- src/hooks/callbacks/staking/useStakingWithdraw.ts
+++ src/hooks/callbacks/staking/useStakingWithdraw.ts
@@ -4,6 +4,7 @@ import { parseUnits } from 'ethers/lib/utils';
 
 import useCore from '../../useCore';
 import useTokenDecimals from '../../useTokenDecimals';
+import { useAddPopup } from '../../../state/application/hooks';
 import { useTransactionAdder } from '../../../state/transactions/hooks';
 
 export default function (
@@ -12,26 +13,37 @@ export default function (
   depositToken: string
 ) {
   const core = useCore();
+  const addPopup = useAddPopup();
   const addTransaction = useTransactionAdder();
   const tokenDecimals = useTokenDecimals(depositToken);
 
   const action = useCallback(async (callback: () => void): Promise<void> => {
     const contract = core.contracts[stakingContract];
-    
-    const response = await contract.withdraw(
-      BigNumber.from(parseUnits(`${amount}`, tokenDecimals))
-    );
 
-    addTransaction(response, {
-      summary: `Withdraw ${amount} ${depositToken}`
-    });
+    try{
+      const response = await contract.withdraw(
+        BigNumber.from(parseUnits(`${amount}`, tokenDecimals))
+      );
 
-    if (callback) callback();
+      addTransaction(response, {
+        summary: `Withdraw ${amount} ${depositToken}`
+      });
+
+      if (callback) callback();
+    } catch(e) {
+      addPopup({
+        error: {
+          message: e.data.message,
+          stack: e.stack
+        }
+      });
+    }
   }, [
     core.contracts, 
     tokenDecimals, 
     stakingContract, 
-    amount, 
+    amount,
+    addPopup,
     addTransaction, 
     depositToken
   ]);
