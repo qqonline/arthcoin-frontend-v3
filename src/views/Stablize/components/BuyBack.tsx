ad63c771ad16eadf84893df8e6240fd8946f7646
Mithil Jain <mithiljain8@gmail.com>
2021-05-27 18:21:37 +0530

    genesis minor

--- src/views/Stablize/components/BuyBack.tsx
+++ src/views/Stablize/components/BuyBack.tsx
@@ -36,8 +36,8 @@ type Iprops = {
 };
 
 const BuyBack = (props: WithSnackbarProps & Iprops) => {
-  const [redeemAmount, setRedeemAmount] = useState<string>('0.00');
-  const [receiveAmount, setReceiveAmount] = useState<string>('0.00');
+  const [redeemAmount, setRedeemAmount] = useState<string>('0');
+  const [receiveAmount, setReceiveAmount] = useState<string>('0');
   const [openModal, setOpenModal] = useState<boolean>(false);
   const [selectedRate, setSelectedRate] = useState<number>(0.0);
   const [isInputFieldError, setIsInputFieldError] = useState<boolean>(false);
@@ -78,7 +78,7 @@ const BuyBack = (props: WithSnackbarProps & Iprops) => {
   const isARTHXApproved = approveStatus === ApprovalState.APPROVED;
   const isWalletConnected = !!account;
   const isARTHXApproving = approveStatus === ApprovalState.PENDING;
- 
+
   if (!core) return <div />;
 
   const handleBuyback = () => {
