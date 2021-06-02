5c9acd5ac42104524f2eb7c6e27deaaae820f179
Yash Agrawal <yashagrawal18@outlook.com>
2021-06-02 14:32:50 +0530

    Minor improvements

--- src/hooks/useCirculatingSupply.ts
+++ src/hooks/useCirculatingSupply.ts
@@ -1,23 +1,18 @@
 import { BigNumber } from 'ethers';
 import { useEffect, useState, useCallback } from 'react';
 
-import useCore from './useCore';
 import useTotalSupply from './useTotalSupply';
+import useUniswapLiquidity from './useUniswapLiquidity';
 
 const useCirculatingSupply = (asset: string) => {
   const [circulatingSupply, setCirculatingSupply] = useState<BigNumber>(BigNumber.from(0));
   
-  const core = useCore();
-  const token = core.tokens[asset];
   const totalSupply = useTotalSupply(asset);
-  const tradingPairs = core.tradingPairs[asset];
+  const uniswapLiquidity = useUniswapLiquidity(asset);
 
   const fetchCashPrice = useCallback(async () => {
-    const pairBalances = await Promise.all(tradingPairs.map(pair => token.balanceOf(pair.address)));
-    const totalBalance = pairBalances.reduce((a, b) => a.add(b), BigNumber.from(0));
-
-    setCirculatingSupply(totalSupply.sub(totalBalance));
-  }, [token, totalSupply, tradingPairs]);
+    setCirculatingSupply(totalSupply.sub(uniswapLiquidity));
+  }, [uniswapLiquidity, totalSupply]);
 
   useEffect(() => {
     fetchCashPrice().catch((err) =>
