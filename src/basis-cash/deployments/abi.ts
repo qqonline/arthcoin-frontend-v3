e8d0c71daf281ec6455d1d986fdb0bf2e07c1e9e
Steven Enamakel <enamakel@vezures.com>
2021-06-21 07:10:28 +0300

    fixed broken abi

--- src/basis-cash/deployments/abi.ts
+++ src/basis-cash/deployments/abi.ts
@@ -1,30 +1,29 @@
-export const IWETH = require('./abi/IWETH.json');
-export const Oracle = require('./abi/Oracle.json');
-export const Faucet = require('./abi/Faucet.json');
-export const IERC20 = require('./abi/IERC20.json');
-export const Genesis = require('./abi/Genesis.json');
+export const ArthController = require('./abi/ArthController.json');
 export const ArthPool = require('./abi/ArthPool.json');
-export const MahaToken = require('./abi/MahaToken.json');
-export const GMUOracle = require('./abi/GMUOracle.json');
-export const PoolToken = require('./abi/PoolToken.json');
+export const ArthPoolLibrary = require('./abi/ArthPoolLibrary.json');
+export const ArthPoolRouter = require('./abi/ArthPoolRouter.json');
 export const ARTHShares = require('./abi/ARTHShares.json');
-export const LotteryRaffle = require('./abi/LotteryRaffle.json');
-export const UniswapV2Pair = require('./abi/UniswapV2Pair.json');
 export const ARTHStablecoin = require('./abi/ARTHStablecoin.json');
 export const BasicStaking = require('./abi/BasicStaking.json');
-export const ArthPoolRouter = require('./abi/ArthPoolRouter.json');
-export const ArthController = require('./abi/ArthController.json');
-export const ArthPoolLibrary = require('./abi/ArthPoolLibrary.json');
-export const UniswapV2Factory = require('./abi/UniswapV2Factory.json');
+export const Faucet = require('./abi/Faucet.json');
+export const Genesis = require('./abi/Genesis.json');
+export const GMUOracle = require('./abi/GMUOracle.json');
+export const IERC20 = require('./abi/IERC20.json');
+export const IWETH = require('./abi/IWETH.json');
+export const LotteryRaffle = require('./abi/LotteryRaffle.json');
+export const MahaToken = require('./abi/MahaToken.json');
+export const PoolToken = require('./abi/PoolToken.json');
 export const UniswapPairOracle = require('./abi/UniswapPairOracle.json');
+export const UniswapV2Factory = require('./abi/UniswapV2Factory.json');
+export const UniswapV2Pair = require('./abi/UniswapV2Pair.json');
 export const UniswapV2Router02 = require('./abi/UniswapV2Router02.json');
+export const UniversalGMUOracle = require('./abi/UniversalGMUOracle.json');
 
 type IABIS = {
-  [key: string]: any[]
-}
+  [key: string]: any[];
+};
 
 const ret: IABIS = {
-  Genesis,
   ArthController,
   ArthPool,
   ArthPoolLibrary,
@@ -33,17 +32,18 @@ const ret: IABIS = {
   ARTHStablecoin,
   BasicStaking,
   Faucet,
-  PoolToken,
+  Genesis,
   GMUOracle,
   IERC20,
   IWETH,
+  LotteryRaffle,
   MahaToken,
-  Oracle,
+  PoolToken,
   UniswapPairOracle,
   UniswapV2Factory,
   UniswapV2Pair,
   UniswapV2Router02,
-  LotteryRaffle
-}
+  UniversalGMUOracle,
+};
 
 export default ret;
