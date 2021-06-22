40310e5c1919bd38b52ba8965f8440f515c3ba4d
Steven Enamakel <enamakel@vezures.com>
2021-06-22 19:24:08 +0300

    patched network version

--- src/hooks/useConfig.ts
+++ src/hooks/useConfig.ts
@@ -4,11 +4,14 @@ const useConfig = () => {
   const configs = Object.values(config);
 
   // @ts-ignore
-  const currentNetworkId = Number(window.ethereum.networkVersion) || 1;
-  const currentConfig = configs.find((c) => Number(c.chainId) === Number(currentNetworkId));
+  if (window.ethereum) {
+    const currentNetworkId = Number(window.ethereum.networkVersion) || 1;
+    const currentConfig = configs.find((c) => Number(c.chainId) === Number(currentNetworkId));
+    if (!currentConfig) return config.ethereum;
+    return currentConfig;
+  }
 
-  if (!currentConfig) return config.ethereum;
-  return currentConfig;
+  return config.ethereum;
 };
 
 export default useConfig;
