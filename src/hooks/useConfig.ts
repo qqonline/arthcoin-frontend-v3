6b28b2f7c2e7c56ad8b9ea98575f689ff0e79cef
Steven Enamakel <enamakel@vezures.com>
2021-06-22 18:54:43 +0300

    udpate cnofig

--- src/hooks/useConfig.ts
+++ src/hooks/useConfig.ts
@@ -4,10 +4,10 @@ const useConfig = () => {
   const configs = Object.values(config);
 
   // @ts-ignore
-  const currentNetworkId = Number(window.ethereum.networkVersion) || 137;
+  const currentNetworkId = Number(window.ethereum.networkVersion) || 1;
   const currentConfig = configs.find((c) => Number(c.chainId) === Number(currentNetworkId));
 
-  if (!currentConfig) return config.matic;
+  if (!currentConfig) return config.ethereum;
   return currentConfig;
 };
 
