c729075dd0e4680093f1c62ab082b96c26760bb7
mithil-jain <mithiljain8@gmail.com>
2021-06-23 18:05:13 +0530

    added pool and trade back

---
+++ src/views/Trade/components/InfoDiv.tsx
@@ -0,0 +1,137 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import InfoIcon from '@material-ui/icons/Info';
+
+import CountUp from 'react-countup';
+
+interface IProps {
+  labelData: string;
+  labelToolTipData?: string;
+  rightLabelValue?: string;
+  rightLabelUnit?: string;
+  countUp?: boolean;
+  cStart?: number;
+  cEnd?: number;
+  cDuration?: number;
+  cDelay?: number;
+}
+
+const TransparentInfoDiv = (props: IProps) => {
+  return (
+    <TransInfoDiv>
+      <InfoSpan>
+        {props.labelData}
+        {props.labelToolTipData && (
+          <InfoIcon
+            fontSize="small"
+            style={{ marginLeft: 2, marginTop: -2, transform: 'scale(0.8)' }}
+          />
+        )}
+      </InfoSpan>
+
+      <LabelInfoData>
+        {props.rightLabelValue && (
+          <LabelInfoText>
+            {props.countUp ? (
+              <CountUp
+                start={props?.cStart}
+                end={props.cEnd}
+                delay={0}
+                duration={props.cDuration}
+                preserveValue={true}
+                onUpdate={() => {
+                  console.log('test');
+                }}
+              />
+            ) : (
+              props.rightLabelValue
+            )}
+          </LabelInfoText>
+        )}
+        {props.rightLabelUnit && (
+          <LabelInfoDataChip>
+            <LabelInfoDataChipText>{props.rightLabelUnit}</LabelInfoDataChipText>
+          </LabelInfoDataChip>
+        )}
+      </LabelInfoData>
+    </TransInfoDiv>
+  );
+};
+
+const InfoSpan = styled.span`
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 20px;
+  color: rgba(255, 255, 255, 0.64);
+  // margin: 10px 30px;
+  text-align: center;
+`;
+
+const LabelInfoText = styled.div`
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: right;
+  color: rgba(255, 255, 255, 0.88);
+`;
+
+const LabelInfoData = styled.div`
+  // background: yellow;
+  padding: 3px 4px;
+  // height: fit-content;
+  width: fit-content;
+  justify-content: space-between;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 12px;
+  line-height: 130%;
+  color: rgba(255, 255, 255, 0.88);
+`;
+
+const LabelInfoDataChip = styled.div`
+  background: rgba(255, 255, 255, 0.08);
+  border-radius: 4px;
+  padding: 3px 4px;
+  height: fit-content;
+  // justify-content: space-between;
+  display: flex;
+  align-items: center;
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 12px;
+  line-height: 130%;
+  margin: 0px 2px;
+  color: rgba(255, 255, 255, 0.64);
+`;
+
+const LabelInfoDataChipText = styled.div`
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 150%;
+  letter-spacing: 0.08em;
+  text-transform: uppercase;
+  color: rgba(255, 255, 255, 0.64);
+`;
+
+const TransInfoDiv = styled.div`
+  // background: rgba(255, 255, 255, 0.08);
+  // border-radius: 6px;
+  // padding: 6px 4px;
+  height: fit-content;
+  justify-content: space-between;
+  display: flex;
+  align-items: center;
+`;
+export default TransparentInfoDiv;
---
+++ src/views/Trade/index.tsx
@@ -0,0 +1,290 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Container from '../../components/Container';
+import Grid from '@material-ui/core/Grid';
+import uniswapLogo from '../../assets/svg/uniswapLogo.svg';
+import shushiswap from '../../assets/svg/sushiswapLogo.svg';
+import { withSnackbar, WithSnackbarProps } from 'notistack';
+import CallMadeIcon from '@material-ui/icons/CallMade';
+import SellContent from './components/SellContent';
+import BuyContent from './components/BuyContent';
+
+const Boardrooms = (props: WithSnackbarProps) => {
+  useEffect(() => window.scrollTo(0, 0), []);
+
+  const [type, setType] = useState<'Buy' | 'Sell'>('Buy');
+
+  const [selectedSwap, setSelectedSwap] = useState<'Uniswap' | 'Sushiswap'>('Uniswap');
+
+  // const isLaunched = Date.now() >= config.boardroomLaunchesAt.getTime();
+  // if (!basisCash) return <div />;
+
+  const TabContent = () => {
+    return (
+      <Grid container style={{ marginTop: '24px' }}>
+        <Grid item lg={3} sm={'auto'}></Grid>
+        <Grid item lg={6} md={12} sm={12} xs={12}>
+          <LeftTopCard className={'custom-mahadao-container'}>
+            <LeftTopCardHeader className={'custom-mahadao-container-header'}>
+              <TabContainer onClick={() => setType('Buy')}>
+                {type === 'Buy' && <ActiveTab />}
+                <TabText>Buy</TabText>
+              </TabContainer>
+              <TabContainer onClick={() => setType('Sell')}>
+                {type === 'Sell' && <ActiveTab />}
+                <TabText>Sell</TabText>
+              </TabContainer>
+            </LeftTopCardHeader>
+            {type === 'Buy' && <BuyContent />}
+            {type === 'Sell' && <SellContent />}
+          </LeftTopCard>
+        </Grid>
+        <Grid item lg={3} sm={'auto'}></Grid>
+      </Grid>
+    );
+  };
+
+  return (
+    <>
+      <GradientDiv />
+      <Container size="lg">
+        <div>
+          <PageHeading>TRADE</PageHeading>
+          <PageSubHeading>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </PageSubHeading>
+        </div>
+        <Grid container>
+          <Grid item lg={3}></Grid>
+          <Grid item lg={6} md={12} sm={12} xs={12}>
+            <RadioSelectionConatiner>
+              <RadioSubConatiner
+                onClick={() => {
+                  if (selectedSwap === 'Sushiswap') {
+                    setSelectedSwap('Uniswap');
+                  }
+                }}
+              >
+                {selectedSwap === 'Uniswap' && <ActiveRadio />}
+                <RadioText>
+                  <RadioLogo>
+                    <img src={uniswapLogo} style={{ marginTop: '-6px' }} />
+                  </RadioLogo>
+                  Uniswap
+                </RadioText>
+              </RadioSubConatiner>
+              <RadioSubConatiner
+                onClick={() => {
+                  if (selectedSwap === 'Uniswap') {
+                    setSelectedSwap('Sushiswap');
+                  }
+                }}
+              >
+                {selectedSwap === 'Sushiswap' && <ActiveRadio />}
+                <RadioText>
+                  <RadioLogo>
+                    <img src={shushiswap} />
+                  </RadioLogo>
+                  Sushiswap
+                </RadioText>
+              </RadioSubConatiner>
+            </RadioSelectionConatiner>
+          </Grid>
+          <Grid item lg={3}></Grid>
+        </Grid>
+        {TabContent()}
+        <Grid container style={{ marginTop: '16px' }}>
+          <Grid item lg={3} sm={'auto'}></Grid>
+          <Grid item lg={6} md={12} sm={12} xs={12}>
+            <CustomInfoCard className={'custom-mahadao-box'}>
+              <CustomInfoCardDetails>
+                <Grid container>
+                  <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                    <InfoBoxTitle>ETH-ARTH Uniswap pool</InfoBoxTitle>
+                    <InfoBoxSubTitle>Provide liquidity to ETH-ARTH on uniswap</InfoBoxSubTitle>
+                  </Grid>
+                </Grid>
+              </CustomInfoCardDetails>
+              <CustomInfoCardButton>
+                <img src={uniswapLogo} style={{ marginTop: '-6px', marginRight: '10px' }} />
+                <span>Add liquidity on Uniswap</span>
+                <CallMadeIcon style={{ fontSize: 15, marginLeft: '10px' }} />
+              </CustomInfoCardButton>
+            </CustomInfoCard>
+          </Grid>
+          <Grid item lg={3} sm={'auto'}></Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+}
+
+const GradientDiv = styled.div`
+  background: linear-gradient(180deg, #2a2827 0%, rgba(42, 40, 39, 0) 100%);
+  height: 270px;
+  position: absolute;
+  // border: 1px solid;
+  width: 100%;
+  z-index: -5;
+`;
+
+const CustomInfoCardButton = styled.div`
+  width: 100%;
+  border: 1px solid rgba(255, 255, 255, 0.32);
+  box-sizing: border-box;
+  border-radius: 6px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 12px;
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 20px;
+  cursor: pointer;
+  &:hover {
+    background: #423b38;
+  }
+  /* identical to box height, or 143% */
+
+  text-align: center;
+
+  color: #ffffff;
+
+  opacity: 0.88;
+`;
+
+const InfoBoxTitle = styled.p`
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+  color: #ffffff;
+  margin: 0;
+`;
+
+const InfoBoxSubTitle = styled.p`
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 150%;
+  color: rgba(255, 255, 255, 0.64);
+  margin: 0;
+`;
+
+const PageHeading = styled.p`
+  font-family: Syne;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 29px;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  text-align: center;
+  color: #ffffff;
+  margin-top: 40px;
+`;
+
+const PageSubHeading = styled.p`
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 150%;
+  color: rgba(255, 255, 255, 0.64);
+  text-align: center;
+  margin-bottom: 40px;
+`;
+
+const RadioSelectionConatiner = styled.div`
+  background: #2a2827;
+  border-radius: 8px;
+  padding: 6px;
+  display: flex;
+  flex-direction: row;
+`;
+const RadioSubConatiner = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40px;
+  z-index: 1;
+  cursor: pointer;
+  flex: 0.5;
+  position: relative;
+`;
+
+const RadioText = styled.span`
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  color: rgba(255, 255, 255, 0.88);
+  z-index: 1;
+`;
+
+const RadioLogo = styled.span`
+  margin-left: 5px;
+  margin-right: 5px;
+`;
+
+const ActiveRadio = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 40px;
+  background: #423b38;
+  border-radius: 4px;
+  z-index: 0;
+`;
+
+const LeftTopCard = styled.div``;
+
+const LeftTopCardHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const TabContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 32px 12px;
+  width: 100px;
+  height: 80px;
+  z-index: 1;
+  cursor: pointer;
+  flex: 0.5;
+  position: relative;
+`;
+
+const TabText = styled.span`
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  color: rgba(255, 255, 255, 0.88);
+`;
+
+const ActiveTab = styled.div`
+  position: absolute;
+  width: 100%;
+  padding: 32px 12px;
+  background: linear-gradient(180deg, rgba(244, 127, 87, 0) 0%, #fd565620);
+  height: 80px;
+  z-index: 0;
+  border-bottom: 2px solid #fd5656;
+`;
+
+const CustomInfoCard = styled.div``;
+
+const CustomInfoCardDetails = styled.div`
+  margin: 10px 0;
+`;
+
+export default withSnackbar(Boardrooms);
